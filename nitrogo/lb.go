e648f85299c9172e3238b21b0bf77cce97b35b6b
AdamJCrawford <adamcrawford253@gmail.com>
2025-04-19 18:36:58 -0500

    2 more boilerplate.

--- nitrogo/lb.go
+++ nitrogo/lb.go
@@ -1,5 +1,361 @@
 package nitrogo
 
+// Load Balancing configuration. The load balancing methods manage the selection of an appropriate physical server in a server farm.
+// https://developer-docs.netscaler.com/en-us/adc-nitro-api/current-release/configuration/lb/lb
 type LBService struct {
 	client *Client
 }
+
+// lbaction
+// Configuration for lb action resource.
+// https://developer-docs.netscaler.com/en-us/adc-nitro-api/current-release/configuration/lb/lbaction
+func (s *LBService) AddLBAction()    {}
+func (s *LBService) DeleteLBAction() {}
+func (s *LBService) UpdateLBAction() {}
+func (s *LBService) UnsetLBAction()  {}
+func (s *LBService) GetAllLBAction() {}
+func (s *LBService) GetLBAction()    {}
+func (s *LBService) CountLBAction()  {}
+func (s *LBService) RenameLBAction() {}
+
+// lbglobal_binding
+func (s *LBService) GetLBGlobalBinding() {}
+
+// lbglobal_lbpolicy_binding
+func (s *LBService) AddLBGlobalLBPolicyBinding()    {}
+func (s *LBService) DeleteLBGlobalLBPolicyBinding() {}
+func (s *LBService) GetLBGlobalLBPolicyBinding()    {}
+func (s *LBService) CountLBGlobalLBPolicyBinding()  {}
+
+// lbgroup
+func (s *LBService) AddLBGroup()    {}
+func (s *LBService) DeleteLBGroup() {}
+func (s *LBService) UpdateLBGroup() {}
+func (s *LBService) UnsetLBGroup()  {}
+func (s *LBService) GetAllLBGroup() {}
+func (s *LBService) GetLBGroup()    {}
+func (s *LBService) CountLBGroup()  {}
+func (s *LBService) RenameLBGroup() {}
+
+// lbgroup_binding
+func (s *LBService) GetAllLBGroupBinding() {}
+func (s *LBService) GetLBGroupBinding()    {}
+
+// lbgroup_lbvserver_binding
+func (s *LBService) AddLBGroupLBVServerBinding()    {}
+func (s *LBService) DeleteLBGroupLBVServerBinding() {}
+func (s *LBService) GetAllLBGroupLBVServerBinding() {}
+func (s *LBService) GetLBGroupLBVServerBinding()    {}
+func (s *LBService) CountLBGroupLBVServerBinding()  {}
+
+// lbmetrictable
+func (s *LBService) AddLBMetricTable()    {}
+func (s *LBService) DeleteLBMetricTable() {}
+func (s *LBService) UpdateLBMetricTable() {}
+func (s *LBService) GetAllLBMetricTable() {}
+func (s *LBService) GetLBMetricTable()    {}
+func (s *LBService) CountLBMetricTable()  {}
+
+// lbmetrictable_binding
+func (s *LBService) GetAllLBMetricTableBinding() {}
+func (s *LBService) GetLBMetricTableBinding()    {}
+
+// lbmetrictable_metric_binding
+func (s *LBService) AddLBMetricTableMetricBinding()    {}
+func (s *LBService) DeleteLBMetricTableMetricBinding() {}
+func (s *LBService) GetAllLBMetricTableMetricBinding() {}
+func (s *LBService) GetLBMetricTableMetricBinding()    {}
+func (s *LBService) CountLBMetricTableMetricBinding()  {}
+
+// lbmonbindings
+func (s *LBService) GetAllLBMonBindings() {}
+func (s *LBService) GetLBMonBindings()    {}
+func (s *LBService) CountLBMonBindings()  {}
+
+// lbmonbindings_binding
+func (s *LBService) GetLBMonBindingsBinding() {}
+
+// lbmonbindings_gslbservicegroup_binding
+func (s *LBService) GetLBMonBindingsGSLBServiceGroupBinding()   {}
+func (s *LBService) CountLBMonBindingsGSLBServiceGroupBinding() {}
+
+// lbmonbindings_servicegroup_binding
+func (s *LBService) GetLBMonBindingsServiceGroupBinding()   {}
+func (s *LBService) CountLBMonBindingsServiceGroupBinding() {}
+
+// lbmonbindings_service_binding
+func (s *LBService) GetLBMonBindingsServiceBinding()   {}
+func (s *LBService) CountLBMonBindingsServiceBinding() {}
+
+// lbmonitor
+func (s *LBService) AddLBMonitor()     {}
+func (s *LBService) DeleteLBMonitor()  {}
+func (s *LBService) UpdateLBMonitor()  {}
+func (s *LBService) UnsetLBMonitor()   {}
+func (s *LBService) EnableLBMonitor()  {}
+func (s *LBService) DisableLBMonitor() {}
+func (s *LBService) GetAllLBMonitor()  {}
+func (s *LBService) GetLBMonitor()     {}
+func (s *LBService) CountLBMonitor()   {}
+
+// lbmonitor_binding
+func (s *LBService) GetAllLBMonitorBinding() {}
+func (s *LBService) GetLBMonitorBinding()    {}
+
+// lbmonitor_metric_binding
+func (s *LBService) AddLBMonitorMetricBinding()    {}
+func (s *LBService) DeleteLBMonitorMetricBinding() {}
+func (s *LBService) GetAllLBMonitorMetricBinding() {}
+func (s *LBService) GetLBMonitorMetricBinding()    {}
+func (s *LBService) CountLBMonitorMetricBinding()  {}
+
+// lbmonitor_servicegroup_binding
+func (s *LBService) AddLBMonitorServiceGroupBinding()    {}
+func (s *LBService) DeleteLBMonitorServiceGroupBinding() {}
+
+// lbmonitor_service_binding
+func (s *LBService) AddLBMonitorServiceBinding()    {}
+func (s *LBService) DeleteLBMonitorServiceBinding() {}
+
+// lbmonitor_sslcertkey_binding
+func (s *LBService) AddLBMonitorSSLCertKeyBinding()    {}
+func (s *LBService) DeleteLBMonitorSSLCertKeyBinding() {}
+func (s *LBService) GetAllLBMonitorSSLCertKeyBinding() {}
+func (s *LBService) GetLBMonitorSSLCertKeyBinding()    {}
+func (s *LBService) CountLBMonitorSSLCertKeyBinding()  {}
+
+// lbparameter
+func (s *LBService) UpdateLBParameter() {}
+func (s *LBService) UnsetLBParameter()  {}
+func (s *LBService) GetAllLBParameter() {}
+
+// lbpersistentsessions
+func (s *LBService) GetAllLBPersistentSessions() {}
+func (s *LBService) CountLBPersistentSessions()  {}
+func (s *LBService) ClearLBPersistentSessions()  {}
+
+// lbprofile
+func (s *LBService) AddLBProfile()    {}
+func (s *LBService) DeleteLBProfile() {}
+func (s *LBService) UpdateLBProfile() {}
+func (s *LBService) UnsetLBProfile()  {}
+func (s *LBService) GetAllLBProfile() {}
+func (s *LBService) GetLBProfile()    {}
+func (s *LBService) CountLBProfile()  {}
+
+// lbroute
+func (s *LBService) AddLBRoute()    {}
+func (s *LBService) DeleteLBRoute() {}
+func (s *LBService) GetAllLBRoute() {}
+func (s *LBService) CountLBRoute()  {}
+
+// lbroute6
+func (s *LBService) AddLBRoute6()    {}
+func (s *LBService) DeleteLBRoute6() {}
+func (s *LBService) GetAllLBRoute6() {}
+func (s *LBService) CountLBRoute6()  {}
+
+// lbsipparameters
+func (s *LBService) UpdateLBSIPParameters() {}
+func (s *LBService) UnsetLBSIPParameters()  {}
+func (s *LBService) GetAllLBSIPParameters() {}
+
+// lbvserver
+func (s *LBService) AddLBVServer()     {}
+func (s *LBService) DeleteLBVServer()  {}
+func (s *LBService) UpdateLBVServer()  {}
+func (s *LBService) UnsetLBVServer()   {}
+func (s *LBService) EnableLBVServer()  {}
+func (s *LBService) DisableLBVServer() {}
+func (s *LBService) GetAllLBVServer()  {}
+func (s *LBService) GetLBVServer()     {}
+func (s *LBService) CountLBVServer()   {}
+func (s *LBService) RenameLBVServer()  {}
+
+// lbvserver_analyticsprofile_binding
+func (s *LBService) AddLBVServerAnalyticsProfileBinding()    {}
+func (s *LBService) DeleteLBVServerAnalyticsProfileBinding() {}
+func (s *LBService) GetAllLBVServerAnalyticsProfileBinding() {}
+func (s *LBService) GetLBVServerAnalyticsProfileBinding()    {}
+func (s *LBService) CountLBVServerAnalyticsProfileBinding()  {}
+
+// lbvserver_appflowpolicy_binding
+func (s *LBService) AddLBVServerAppFlowPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerAppFlowPolicyBinding() {}
+func (s *LBService) GetAllLBVServerAppFlowPolicyBinding() {}
+func (s *LBService) GetLBVServerAppFlowPolicyBinding()    {}
+func (s *LBService) CountLBVServerAppFlowPolicyBinding()  {}
+
+// lbvserver_appfwpolicy_binding
+func (s *LBService) AddLBVServerAppFWPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerAppFWPolicyBinding() {}
+func (s *LBService) GetAllLBVServerAppFWPolicyBinding() {}
+func (s *LBService) GetLBVServerAppFWPolicyBinding()    {}
+func (s *LBService) CountLBVServerAppFWPolicyBinding()  {}
+
+// lbvserver_appqoepolicy_binding
+func (s *LBService) AddLBVServerAppQOEPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerAppQOEPolicyBinding() {}
+func (s *LBService) GetAllLBVServerAppQOEPolicyBinding() {}
+func (s *LBService) GetLBVServerAppQOEPolicyBinding()    {}
+func (s *LBService) CountLBVServerAppQOEPolicyBinding()  {}
+
+// lbvserver_auditnslogpolicy_binding
+func (s *LBService) AddLBVServerAuditNSLogPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerAuditNSLogPolicyBinding() {}
+func (s *LBService) GetAllLBVServerAuditNSLogPolicyBinding() {}
+func (s *LBService) GetLBVServerAuditNSLogPolicyBinding()    {}
+func (s *LBService) CountLBVServerAuditNSLogPolicyBinding()  {}
+
+// lbvserver_auditsyslogpolicy_binding
+func (s *LBService) AddLBVServerAuditSyslogPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerAuditSyslogPolicyBinding() {}
+func (s *LBService) GetAllLBVServerAuditSyslogPolicyBinding() {}
+func (s *LBService) GetLBVServerAuditSyslogPolicyBinding()    {}
+func (s *LBService) CountLBVServerAuditSyslogPolicyBinding()  {}
+
+// lbvserver_authorizationpolicy_binding
+func (s *LBService) AddLBVServerAuthorizationPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerAuthorizationPolicyBinding() {}
+func (s *LBService) GetAllLBVServerAuthorizationPolicyBinding() {}
+func (s *LBService) GetLBVServerAuthorizationPolicyBinding()    {}
+func (s *LBService) CountLBVServerAuthorizationPolicyBinding()  {}
+
+// lbvserver_binding
+func (s *LBService) GetAllLBVServerBinding() {}
+func (s *LBService) GetLBVServerBinding()    {}
+
+// lbvserver_botpolicy_binding
+func (s *LBService) AddLBVServerBotPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerBotPolicyBinding() {}
+func (s *LBService) GetAllLBVServerBotPolicyBinding() {}
+func (s *LBService) GetLBVServerBotPolicyBinding()    {}
+func (s *LBService) CountLBVServerBotPolicyBinding()  {}
+
+// lbvserver_cachepolicy_binding
+func (s *LBService) AddLBVServerCachePolicyBinding()    {}
+func (s *LBService) DeleteLBVServerCachePolicyBinding() {}
+func (s *LBService) GetAllLBVServerCachePolicyBinding() {}
+func (s *LBService) GetLBVServerCachePolicyBinding()    {}
+func (s *LBService) CountLBVServerCachePolicyBinding()  {}
+
+// lbvserver_cmppolicy_binding
+func (s *LBService) AddLBVServerCMPPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerCMPPolicyBinding() {}
+func (s *LBService) GetAllLBVServerCMPPolicyBinding() {}
+func (s *LBService) GetLBVServerCMPPolicyBinding()    {}
+func (s *LBService) CountLBVServerCMPPolicyBinding()  {}
+
+// lbvserver_contentinspectionpolicy_binding
+func (s *LBService) AddLBVServerContentInspectionPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerContentInspectionPolicyBinding() {}
+func (s *LBService) GetAllLBVServerContentInspectionPolicyBinding() {}
+func (s *LBService) GetLBVServerContentInspectionPolicyBinding()    {}
+func (s *LBService) CountLBVServerContentInspectionPolicyBinding()  {}
+
+// lbvserver_csvserver_binding
+func (s *LBService) GetAllLBVServerCSVServerBinding() {}
+func (s *LBService) GetLBVServerCSVServerBinding()    {}
+func (s *LBService) CountLBVServerCSVServerBinding()  {}
+
+// lbvserver_dnspolicy64_binding
+func (s *LBService) AddLBVServerDNSPolicy64Binding()    {}
+func (s *LBService) DeleteLBVServerDNSPolicy64Binding() {}
+func (s *LBService) GetAllLBVServerDNSPolicy64Binding() {}
+func (s *LBService) GetLBVServerDNSPolicy64Binding()    {}
+func (s *LBService) CountLBVServerDNSPolicy64Binding()  {}
+
+// lbvserver_feopolicy_binding
+func (s *LBService) AddLBVServerFEOPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerFEOPolicyBinding() {}
+func (s *LBService) GetAllLBVServerFEOPolicyBinding() {}
+func (s *LBService) GetLBVServerFEOPolicyBinding()    {}
+func (s *LBService) CountLBVServerFEOPolicyBinding()  {}
+
+// lbvserver_responderpolicy_binding
+func (s *LBService) AddLBVServerResponderPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerResponderPolicyBinding() {}
+func (s *LBService) GetAllLBVServerResponderPolicyBinding() {}
+func (s *LBService) GetLBVServerResponderPolicyBinding()    {}
+func (s *LBService) CountLBVServerResponderPolicyBinding()  {}
+
+// lbvserver_rewritepolicy_binding
+func (s *LBService) AddLBVServerRewritePolicyBinding()    {}
+func (s *LBService) DeleteLBVServerRewritePolicyBinding() {}
+func (s *LBService) GetAllLBVServerRewritePolicyBinding() {}
+func (s *LBService) GetLBVServerRewritePolicyBinding()    {}
+func (s *LBService) CountLBVServerRewritePolicyBinding()  {}
+
+// lbvserver_servicegroupmember_binding
+func (s *LBService) GetAllLBVServerServiceGroupMemberBinding() {}
+func (s *LBService) GetLBVServerServiceGroupMemberBinding()    {}
+func (s *LBService) CountLBVServerServiceGroupMemberBinding()  {}
+
+// lbvserver_servicegroup_binding
+func (s *LBService) AddLBVServerServiceGroupBinding()    {}
+func (s *LBService) DeleteLBVServerServiceGroupBinding() {}
+func (s *LBService) GetAllLBVServerServiceGroupBinding() {}
+func (s *LBService) GetLBVServerServiceGroupBinding()    {}
+func (s *LBService) CountLBVServerServiceGroupBinding()  {}
+
+// lbvserver_service_binding
+func (s *LBService) AddLBVServerServiceBinding()    {}
+func (s *LBService) DeleteLBVServerServiceBinding() {}
+func (s *LBService) GetAllLBVServerServiceBinding() {}
+func (s *LBService) GetLBVServerServiceBinding()    {}
+func (s *LBService) CountLBVServerServiceBinding()  {}
+
+// lbvserver_spilloverpolicy_binding
+func (s *LBService) AddLBVServerSpilloverPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerSpilloverPolicyBinding() {}
+func (s *LBService) GetAllLBVServerSpilloverPolicyBinding() {}
+func (s *LBService) GetLBVServerSpilloverPolicyBinding()    {}
+func (s *LBService) CountLBVServerSpilloverPolicyBinding()  {}
+
+// lbvserver_tmtrafficpolicy_binding
+func (s *LBService) AddLBVServerTMTrafficPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerTMTrafficPolicyBinding() {}
+func (s *LBService) GetAllLBVServerTMTrafficPolicyBinding() {}
+func (s *LBService) GetLBVServerTMTrafficPolicyBinding()    {}
+func (s *LBService) CountLBVServerTMTrafficPolicyBinding()  {}
+
+// lbvserver_transformpolicy_binding
+func (s *LBService) AddLBVServerTransformPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerTransformPolicyBinding() {}
+func (s *LBService) GetAllLBVServerTransformPolicyBinding() {}
+func (s *LBService) GetLBVServerTransformPolicyBinding()    {}
+func (s *LBService) CountLBVServerTransformPolicyBinding()  {}
+
+// lbvserver_videooptimizationdetectionpolicy_binding
+func (s *LBService) AddLBVServerVideoOptimizationDetectionPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerVideoOptimizationDetectionPolicyBinding() {}
+func (s *LBService) GetAllLBVServerVideoOptimizationDetectionPolicyBinding() {}
+func (s *LBService) GetLBVServerVideoOptimizationDetectionPolicyBinding()    {}
+func (s *LBService) CountLBVServerVideoOptimizationDetectionPolicyBinding()  {}
+
+// lbvserver_videooptimizationpacingpolicy_binding
+func (s *LBService) AddLBVServerVideoOptimizationPacingPolicyBinding()    {}
+func (s *LBService) DeleteLBVServerVideoOptimizationPacingPolicyBinding() {}
+func (s *LBService) GetAllLBVServerVideoOptimizationPacingPolicyBinding() {}
+func (s *LBService) GetLBVServerVideoOptimizationPacingPolicyBinding()    {}
+func (s *LBService) CountLBVServerVideoOptimizationPacingPolicyBinding()  {}
+
+// lbwlm
+func (s *LBService) AddLBWLM()    {}
+func (s *LBService) DeleteLBWLM() {}
+func (s *LBService) UpdateLBWLM() {}
+func (s *LBService) UnsetLBWLM()  {}
+func (s *LBService) GetAllLBWLM() {}
+func (s *LBService) GetLBWLM()    {}
+func (s *LBService) CountLBWLM()  {}
+
+// lbwlm_binding
+func (s *LBService) GetAllLBWLMBinding() {}
+func (s *LBService) GetLBWLMBinding()    {}
+
+// lbwlm_lbvserver_binding
+func (s *LBService) AddLBWLMLBVServerBinding()    {}
+func (s *LBService) DeleteLBWLMLBVServerBinding() {}
+func (s *LBService) GetAllLBWLMLBVServerBinding() {}
+func (s *LBService) GetLBWLMLBVServerBinding()    {}
+func (s *LBService) CountLBWLMLBVServerBinding()  {}
--- nitrogo/lsn.go
+++ nitrogo/lsn.go
@@ -1,5 +1,285 @@
 package nitrogo
 
+// Large Scale NAT commands
 type LSNService struct {
 	client *Client
 }
+
+// lsnappsattributes
+func (s *LSNService) AddLSNAppsAttributes()    {}
+func (s *LSNService) DeleteLSNAppsAttributes() {}
+func (s *LSNService) UpdateLSNAppsAttributes() {}
+func (s *LSNService) UnsetLSNAppsAttributes()  {}
+func (s *LSNService) GetAllLSNAppsAttributes() {}
+func (s *LSNService) GetLSNAppsAttributes()    {}
+func (s *LSNService) CountLSNAppsAttributes()  {}
+
+// lsnappsprofile
+func (s *LSNService) AddLSNAppsProfile()    {}
+func (s *LSNService) DeleteLSNAppsProfile() {}
+func (s *LSNService) UpdateLSNAppsProfile() {}
+func (s *LSNService) UnsetLSNAppsProfile()  {}
+func (s *LSNService) GetAllLSNAppsProfile() {}
+func (s *LSNService) GetLSNAppsProfile()    {}
+func (s *LSNService) CountLSNAppsProfile()  {}
+
+// lsnappsprofile_binding
+func (s *LSNService) GetAllLSNAppsProfileBinding() {}
+func (s *LSNService) GetLSNAppsProfileBinding()    {}
+
+// lsnappsprofile_lsnappsattributes_binding
+func (s *LSNService) AddLSNAppsProfileLSNAppsAttributesBinding()    {}
+func (s *LSNService) DeleteLSNAppsProfileLSNAppsAttributesBinding() {}
+func (s *LSNService) GetAllLSNAppsProfileLSNAppsAttributesBinding() {}
+func (s *LSNService) GetLSNAppsProfileLSNAppsAttributesBinding()    {}
+func (s *LSNService) CountLSNAppsProfileLSNAppsAttributesBinding()  {}
+
+// lsnappsprofile_port_binding
+func (s *LSNService) AddLSNAppsProfilePortBinding()    {}
+func (s *LSNService) DeleteLSNAppsProfilePortBinding() {}
+func (s *LSNService) GetAllLSNAppsProfilePortBinding() {}
+func (s *LSNService) GetLSNAppsProfilePortBinding()    {}
+func (s *LSNService) CountLSNAppsProfilePortBinding()  {}
+
+// lsnclient
+func (s *LSNService) AddLSNClient()    {}
+func (s *LSNService) DeleteLSNClient() {}
+func (s *LSNService) GetAllLSNClient() {}
+func (s *LSNService) GetLSNClient()    {}
+func (s *LSNService) CountLSNClient()  {}
+
+// lsnclient_binding
+func (s *LSNService) GetAllLSNClientBinding() {}
+func (s *LSNService) GetLSNClientBinding()    {}
+
+// lsnclient_network6_binding
+func (s *LSNService) AddLSNClientNetwork6Binding()    {}
+func (s *LSNService) DeleteLSNClientNetwork6Binding() {}
+func (s *LSNService) GetAllLSNClientNetwork6Binding() {}
+func (s *LSNService) GetLSNClientNetwork6Binding()    {}
+func (s *LSNService) CountLSNClientNetwork6Binding()  {}
+
+// lsnclient_network_binding
+func (s *LSNService) AddLSNClientNetworkBinding()    {}
+func (s *LSNService) DeleteLSNClientNetworkBinding() {}
+func (s *LSNService) GetAllLSNClientNetworkBinding() {}
+func (s *LSNService) GetLSNClientNetworkBinding()    {}
+func (s *LSNService) CountLSNClientNetworkBinding()  {}
+
+// lsnclient_nsacl6_binding
+func (s *LSNService) AddLSNClientNSACL6Binding()    {}
+func (s *LSNService) DeleteLSNClientNSACL6Binding() {}
+func (s *LSNService) GetAllLSNClientNSACL6Binding() {}
+func (s *LSNService) GetLSNClientNSACL6Binding()    {}
+func (s *LSNService) CountLSNClientNSACL6Binding()  {}
+
+// lsnclient_nsacl_binding
+func (s *LSNService) AddLSNClientNSACLBinding()    {}
+func (s *LSNService) DeleteLSNClientNSACLBinding() {}
+func (s *LSNService) GetAllLSNClientNSACLBinding() {}
+func (s *LSNService) GetLSNClientNSACLBinding()    {}
+func (s *LSNService) CountLSNClientNSACLBinding()  {}
+
+// lsndeterministicnat
+func (s *LSNService) GetAllLSNDeterministicNAT() {}
+func (s *LSNService) CountLSNDeterministicNAT()  {}
+
+// lsngroup
+func (s *LSNService) AddLSNGroup()    {}
+func (s *LSNService) DeleteLSNGroup() {}
+func (s *LSNService) UpdateLSNGroup() {}
+func (s *LSNService) UnsetLSNGroup()  {}
+func (s *LSNService) GetAllLSNGroup() {}
+func (s *LSNService) GetLSNGroup()    {}
+func (s *LSNService) CountLSNGroup()  {}
+
+// lsngroup_binding
+func (s *LSNService) GetAllLSNGroupBinding() {}
+func (s *LSNService) GetLSNGroupBinding()    {}
+
+// lsngroup_ipsecalgprofile_binding
+func (s *LSNService) AddLSNGroupIPSecALGProfileBinding()    {}
+func (s *LSNService) DeleteLSNGroupIPSecALGProfileBinding() {}
+func (s *LSNService) GetAllLSNGroupIPSecALGProfileBinding() {}
+func (s *LSNService) GetLSNGroupIPSecALGProfileBinding()    {}
+func (s *LSNService) CountLSNGroupIPSecALGProfileBinding()  {}
+
+// lsngroup_lsnappsprofile_binding
+func (s *LSNService) AddLSNGroupLSNAppsProfileBinding()    {}
+func (s *LSNService) DeleteLSNGroupLSNAppsProfileBinding() {}
+func (s *LSNService) GetAllLSNGroupLSNAppsProfileBinding() {}
+func (s *LSNService) GetLSNGroupLSNAppsProfileBinding()    {}
+func (s *LSNService) CountLSNGroupLSNAppsProfileBinding()  {}
+
+// lsngroup_lsnhttphdrlogprofile_binding
+func (s *LSNService) AddLSNGroupLSNHTTPHDRLogProfileBinding()    {}
+func (s *LSNService) DeleteLSNGroupLSNHTTPHDRLogProfileBinding() {}
+func (s *LSNService) GetAllLSNGroupLSNHTTPHDRLogProfileBinding() {}
+func (s *LSNService) GetLSNGroupLSNHTTPHDRLogProfileBinding()    {}
+func (s *LSNService) CountLSNGroupLSNHTTPHDRLogProfileBinding()  {}
+
+// lsngroup_lsnlogprofile_binding
+func (s *LSNService) AddLSNGroupLSNLogProfileBinding()    {}
+func (s *LSNService) DeleteLSNGroupLSNLogProfileBinding() {}
+func (s *LSNService) GetAllLSNGroupLSNLogProfileBinding() {}
+func (s *LSNService) GetLSNGroupLSNLogProfileBinding()    {}
+func (s *LSNService) CountLSNGroupLSNLogProfileBinding()  {}
+
+// lsngroup_lsnpool_binding
+func (s *LSNService) AddLSNGroupLSNPoolBinding()    {}
+func (s *LSNService) DeleteLSNGroupLSNPoolBinding() {}
+func (s *LSNService) GetAllLSNGroupLSNPoolBinding() {}
+func (s *LSNService) GetLSNGroupLSNPoolBinding()    {}
+func (s *LSNService) CountLSNGroupLSNPoolBinding()  {}
+
+// lsngroup_lsnrtspalgprofile_binding
+func (s *LSNService) AddLSNGroupLSNRTSPALGProfileBinding()    {}
+func (s *LSNService) DeleteLSNGroupLSNRTSPALGProfileBinding() {}
+func (s *LSNService) GetAllLSNGroupLSNRTSPALGProfileBinding() {}
+func (s *LSNService) GetLSNGroupLSNRTSPALGProfileBinding()    {}
+func (s *LSNService) CountLSNGroupLSNRTSPALGProfileBinding()  {}
+
+// lsngroup_lsnsipalgprofile_binding
+func (s *LSNService) AddKSBGroupLSNSIPALGProfileBinding()    {}
+func (s *LSNService) DeleteKSBGroupLSNSIPALGProfileBinding() {}
+func (s *LSNService) GetAllKSBGroupLSNSIPALGProfileBinding() {}
+func (s *LSNService) GetKSBGroupLSNSIPALGProfileBinding()    {}
+func (s *LSNService) CountKSBGroupLSNSIPALGProfileBinding()  {}
+
+// lsngroup_lsntransportprofile_binding
+func (s *LSNService) AddLSNGroupLSNTransportProfileBinding()    {}
+func (s *LSNService) DeleteLSNGroupLSNTransportProfileBinding() {}
+func (s *LSNService) GetAllLSNGroupLSNTransportProfileBinding() {}
+func (s *LSNService) GetLSNGroupLSNTransportProfileBinding()    {}
+func (s *LSNService) CountLSNGroupLSNTransportProfileBinding()  {}
+
+// lsngroup_pcpserver_binding
+func (s *LSNService) AddLSNGroupPCPServerBinding()    {}
+func (s *LSNService) DeleteLSNGroupPCPServerBinding() {}
+func (s *LSNService) GetAllLSNGroupPCPServerBinding() {}
+func (s *LSNService) GetLSNGroupPCPServerBinding()    {}
+func (s *LSNService) CountLSNGroupPCPServerBinding()  {}
+
+// lsnhttphdrlogprofile
+func (s *LSNService) AddLSNHTTPHDRLogProfile()    {}
+func (s *LSNService) DeleteLSNHTTPHDRLogProfile() {}
+func (s *LSNService) UpdateLSNHTTPHDRLogProfile() {}
+func (s *LSNService) UnsetLSNHTTPHDRLogProfile()  {}
+func (s *LSNService) GetAllLSNHTTPHDRLogProfile() {}
+func (s *LSNService) GetLSNHTTPHDRLogProfile()    {}
+func (s *LSNService) CountLSNHTTPHDRLogProfile()  {}
+
+// lsnip6profile
+func (s *LSNService) AddLSNIP6Profile()    {}
+func (s *LSNService) DeleteLSNIP6Profile() {}
+func (s *LSNService) GetAllLSNIP6Profile() {}
+func (s *LSNService) GetLSNIP6Profile()    {}
+func (s *LSNService) CountLSNIP6Profile()  {}
+
+// lsnlogprofile
+func (s *LSNService) AddLSNLogProfile()    {}
+func (s *LSNService) DeleteLSNLogProfile() {}
+func (s *LSNService) UpdateLSNLogProfile() {}
+func (s *LSNService) UnsetLSNLogProfile()  {}
+func (s *LSNService) GetAllLSNLogProfile() {}
+func (s *LSNService) GetLSNLogProfile()    {}
+func (s *LSNService) CountLSNLogProfile()  {}
+
+// lsnparameter
+func (s *LSNService) UpdateLSNParameter() {}
+func (s *LSNService) UnsetLSNParameter()  {}
+func (s *LSNService) GetAllLSNParameter() {}
+
+// lsnpool
+func (s *LSNService) AddLSNPool()    {}
+func (s *LSNService) DeleteLSNPool() {}
+func (s *LSNService) UpdateLSNPool() {}
+func (s *LSNService) UnsetLSNPool()  {}
+func (s *LSNService) GetAllLSNPool() {}
+func (s *LSNService) GetLSNPool()    {}
+func (s *LSNService) CountLSNPool()  {}
+
+// lsnpool_binding
+func (s *LSNService) GetAllLSNPoolBinding() {}
+func (s *LSNService) GetLSNPoolBinding()    {}
+
+// lsnpool_lsnip_binding
+func (s *LSNService) AddLSNPoolLSNIPBinding()    {}
+func (s *LSNService) DeleteLSNPoolLSNIPBinding() {}
+func (s *LSNService) GetAllLSNPoolLSNIPBinding() {}
+func (s *LSNService) GetLSNPoolLSNIPBinding()    {}
+func (s *LSNService) CountLSNPoolLSNIPBinding()  {}
+
+// lsnrtspalgprofile
+func (s *LSNService) AddLSNRTSPALGProfile()    {}
+func (s *LSNService) DeleteLSNRTSPALGProfile() {}
+func (s *LSNService) UpdateLSNRTSPALGProfile() {}
+func (s *LSNService) UnsetLSNRTSPALGProfile()  {}
+func (s *LSNService) GetAllLSNRTSPALGProfile() {}
+func (s *LSNService) GetLSNRTSPALGProfile()    {}
+func (s *LSNService) CountLSNRTSPALGProfile()  {}
+
+// lsnrtspalgsession
+func (s *LSNService) GetAllLSNRTSPALGSession() {}
+func (s *LSNService) GetLSNRTSPALGSession()    {}
+func (s *LSNService) CountLSNRTSPALGSession()  {}
+func (s *LSNService) FlushLSNRTSPALGSession()  {}
+
+// lsnrtspalgsession_binding
+func (s *LSNService) GetAllLSNRTSPALGSessionBinding() {}
+func (s *LSNService) GetLSNRTSPALGSessionBinding()    {}
+
+// lsnrtspalgsession_datachannel_binding
+func (s *LSNService) GetAllLSNRTSPALGSessionDataChannelBinding() {}
+func (s *LSNService) GetLSNRTSPALGSessionDataChannelBinding()    {}
+func (s *LSNService) CountLSNRTSPALGSessionDataChannelBinding()  {}
+
+// lsnsession
+func (s *LSNService) GetAllLSNSession() {}
+func (s *LSNService) CountLSNSession()  {}
+func (s *LSNService) FlushLSNSession()  {}
+
+// lsnsipalgcall
+func (s *LSNService) GetAllLSNSIPALGCall() {}
+func (s *LSNService) GetLSNSIPALGCall()    {}
+func (s *LSNService) CountLSNSIPALGCall()  {}
+func (s *LSNService) FlushLSNSIPALGCall()  {}
+
+// lsnsipalgcall_binding
+func (s *LSNService) GetAllLSNSIPALGCallBinding() {}
+func (s *LSNService) GetLSNSIPALGCallBinding()    {}
+
+// lsnsipalgcall_controlchannel_binding
+func (s *LSNService) GetAllLSNSIPALGCallControlChannelBinding() {}
+func (s *LSNService) GetLSNSIPALGCallControlChannelBinding()    {}
+func (s *LSNService) CountLSNSIPALGCallControlChannelBinding()  {}
+
+// lsnsipalgcall_datachannel_binding
+func (s *LSNService) GetAllLSNSIPALGCallDataChannelBinding() {}
+func (s *LSNService) GetLSNSIPALGCallDataChannelBinding()    {}
+func (s *LSNService) CountLSNSIPALGCallDataChannelBinding()  {}
+
+// lsnsipalgprofile
+func (s *LSNService) AddLSNSIPALGProfile()    {}
+func (s *LSNService) DeleteLSNSIPALGProfile() {}
+func (s *LSNService) UpdateLSNSIPALGProfile() {}
+func (s *LSNService) UnsetLSNSIPALGProfile()  {}
+func (s *LSNService) GetAllLSNSIPALGProfile() {}
+func (s *LSNService) GetLSNSIPALGProfile()    {}
+func (s *LSNService) CountLSNSIPALGProfile()  {}
+
+// lsnstatic
+func (s *LSNService) AddLSNStatic()    {}
+func (s *LSNService) DeleteLSNStatic() {}
+func (s *LSNService) GetAllLSNStatic() {}
+func (s *LSNService) GetLSNStatic()    {}
+func (s *LSNService) CountLSNStatic()  {}
+
+// lsntransportprofile
+func (s *LSNService) AddLSNTransportProfile()    {}
+func (s *LSNService) DeleteLSNTransportProfile() {}
+func (s *LSNService) UpdateLSNTransportProfile() {}
+func (s *LSNService) UnsetLSNTransportProfile()  {}
+func (s *LSNService) GetAllLSNTransportProfile() {}
+func (s *LSNService) GetLSNTransportProfile()    {}
+func (s *LSNService) CountLSNTransportProfile()  {}
