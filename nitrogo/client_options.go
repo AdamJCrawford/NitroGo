7f8ab1d6aa1f2c54f066e31d626b27f93c600c69
Adam Crawford <adamcrawford253@gmail.com>
2026-01-21 10:20:56 -0600

    Update client_options.go

--- nitrogo/client_options.go
+++ nitrogo/client_options.go
@@ -25,13 +25,6 @@ func WithHTTPClient(httpClient *http.Client) ClientOptionFunc {
 	}
 }
 
-func WithSessionTimeOut(timeout int) ClientOptionFunc {
-	return func(c *Client) error {
-		c.sessionTimeOut = timeout
-		return nil
-	}
-}
-
 func WithProxiedURL(url string) ClientOptionFunc {
 	return func(c *Client) error {
 		c.proxiedURL = url
